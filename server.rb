356a92f9116c21237b244ef01da357875cd5c95f
Richard Davis <rdavis.bacs@gmail.com>
2015-05-19 13:02:54 -0400

    limit messages to 10

--- server.rb
+++ server.rb
@@ -46,7 +46,7 @@ end
 
 get "/messages" do
   sql = <<-SQL
-    SELECT * FROM messages ORDER BY created_at DESC LIMIT 50;
+    SELECT * FROM messages ORDER BY created_at DESC LIMIT 10;
   SQL
   messages = exec_query(sql)
   erb :"messages/index", locals: { messages: messages }
